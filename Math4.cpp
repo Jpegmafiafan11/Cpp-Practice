68fb665f6e1e6ddcd0df1ed3b464f4318d857ccc
Owen <112315039+Jpegmafiafan11@users.noreply.github.com>
2022-08-30 10:19:50 -0800

    Add files via upload

---
+++ Math4.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int Math(int a, int b) {
+	return a * b + 3;
+}
---
+++ Practice4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int Math(int a, int b);
+
+int main() {
+	cout << Math(5, 7) << endl;
+}
