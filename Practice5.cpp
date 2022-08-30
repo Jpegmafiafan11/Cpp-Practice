10b87a69b88ec5c9b2d9a756a26ae81ab46187cd
Owen <112315039+Jpegmafiafan11@users.noreply.github.com>
2022-08-30 13:22:08 -0800

    Add files via upload

---
+++ Practice5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int numOdds(int n);
+
+int Practice5() {
+	int yourNumber;
+	cout << "Enter a positive integer" << endl;
+	cin >> yourNumber;
+	cout << "The number of odd numbers lower than your int is " << numOdds(yourNumber) << endl;
+	return 0;
+}
+
+int numOdds(int n) {
+	int count = 0;
+	for (int i = n; i > 0; i--) {
+		if (i % 2 == 1) {
+			count++;
+		}
+	}
+	return count;
+}
